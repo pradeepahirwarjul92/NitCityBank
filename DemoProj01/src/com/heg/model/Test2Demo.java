5fc77940aae49f05d58eec38da1010ee437f6489
Developer01 <Dev01@gmail.com>
2025-04-29 10:04:09 +0530

    Test2DemoMethod added

--- DemoProj01/src/com/heg/model/Test2Demo.java
+++ DemoProj01/src/com/heg/model/Test2Demo.java
@@ -1,6 +1,11 @@
 package com.heg.model;
 
 public class Test2Demo {
+	
+	public void addNumber(int a,int b) {
+		System.out.println(a+b);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Test2Demo.main()");
 	}
